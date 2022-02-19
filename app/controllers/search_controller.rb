410488d22308c3f01470282597a5ec2cb62fb2f7
Evangelos Giataganas <e.giataganas+github@gmail.com>
2022-02-19 16:02:51 +0200

    Improvements (#19)
    * Tidy and sort Gemfile alphabetical
    
    * Tidy and configure default rspec configuration
    
    * Fix rubocop, delete rubocop_todo
    
    * Add brakeman
    
    * Add pry-byebug
    
    * Add bundler-audit
    
    * Add bundle_audit, brakeman, rubocop, and spec to the default task
    
    * Update to Rails 7.0.2.2 to address CVE-2022-23633
    
    * Update to Puma 5.6.2 to address CVE-2022-23634
    
    * Run CI on smaller parallel jobs
    
    * Fix rubocop CI failure
    
    The ruby/setup-ruby@v1 action, installs and caches the gems
    automatically.
    
    * Ignore possible unprotected redirect brakeman/warning

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,16 @@
 class SearchController < ApplicationController
   def index
     @search_term = search_term
-    @services = search_term&.length > 1 ? Service.localised_results(search_term, I18n.locale) : []
+    @services = search_term_valid? ? Service.localised_results(search_term, I18n.locale) : []
   end
 
   private
 
   def search_term
     params.permit(:search_term)[:search_term]
+  end
+
+  def search_term_valid?
+    search_term.length > 1
   end
 end
