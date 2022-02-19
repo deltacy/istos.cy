50d05602048fa4a3a15fc2b77278016ffab0e723
Despo Pentara <despo@extractmethod.com>
2022-02-19 00:45:25 +0000

    Fix rubocop warnings and disable  LineLength check for seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-#
-# Topics
-#
 topic_data = {
   benefits: {
     el: { name: 'Επιδόματα', description: 'Επιδόματα και οικονομική υποστήριξη' },
@@ -78,8 +68,10 @@ subtopic_data = {
   },
   benefits: {
     'help-for-families': {
-      el: { name: 'Οικονομική υποστήριξη οικογενειών' , description: 'Οικονομική βοήθεια για οικογένειες και άτομα που προσέχουν παιδιά' },
-      en: { name: 'Financial support for families', description: 'Help if you are looking after children or plan to have or adopt a child' }
+      el: { name: 'Οικονομική υποστήριξη οικογενειών',
+            description: 'Οικονομική βοήθεια για οικογένειες και άτομα που προσέχουν παιδιά' },
+      en: { name: 'Financial support for families',
+            description: 'Help if you are looking after children or plan to have or adopt a child' }
     }
   }
 }
@@ -100,7 +92,7 @@ service_data = {
       description: 'Ανανεώστε την άδειας κυκλοφορίας του οχήματος σας.',
       website: 'https://www.jccsmart.com/change-culture/el?returnUrl=%2Froad-transport-department%2F4888160%2F',
       information: 'Μεταβείτε στην ιστοσελίδα ηλεκτρονικών πληρωμών του Τμήματος Οδικών Μεταφορών και ανανεώστε την ' \
-      'άδεια κυκλοφορίας σας χρησιμοποιώντας την πιστωτική σας κάρτα.',
+                   'άδεια κυκλοφορίας σας χρησιμοποιώντας την πιστωτική σας κάρτα.',
       topics: ['driving-licence']
     },
     en: {
@@ -108,7 +100,7 @@ service_data = {
       description: 'Tax your vehicle.',
       website: 'https://www.jccsmart.com/change-culture/en-GB?returnUrl=%2Froad-transport-department%2F4888160%2F',
       information: "Visit the Road Transport Department's payment website and renew your Road Tax Licence using a " \
-      ' credit card.',
+                   ' credit card.',
       topics: ['driving-licence']
     }
   },
@@ -118,7 +110,7 @@ service_data = {
       description: 'Ανανεώστε την άδειας οδήγησης σας ή εκδώστε καινούρια σε περίπτωση κλοπής ή αλλίωσης.',
       website: 'http://www.mcw.gov.cy/mcw/RTD/rtd.nsf/All/9FE19D004055E3DDC22578240034D9B3?OpenDocument',
       information: 'Μεταβείτε στην ιστοσελίδα του Τμήματος Οδικών Μεταφορών όπου μπορείτε να βρείτε τα απαραίτητα ' \
-      'έντυπα που χρειάζεται να συμπληρώσετε για να πεανεκδόσετε την άδειας οδήγησης σας.',
+                   'έντυπα που χρειάζεται να συμπληρώσετε για να πεανεκδόσετε την άδειας οδήγησης σας.',
       topics: ['driving-licence']
     },
     en: {
@@ -126,7 +118,7 @@ service_data = {
       description: 'Renew your driving licence or issue a new one in case of theft or damanage.',
       website: 'http://www.mcw.gov.cy/mcw/RTD/rtd.nsf/All/9FE19D004055E3DDC22578240034D9B3?OpenDocument',
       information: "Visit the Road Transport Department's website where you can find the relevant documents that you " \
-      'need to submit in order to re-issue your driving licence.',
+                   'need to submit in order to re-issue your driving licence.',
       topics: ['driving-licence']
     }
   },
@@ -136,7 +128,7 @@ service_data = {
       description: '',
       website: 'https://eforms.eservices.cyprus.gov.cy/mlsi/ApplicationSubmission/SUBMENUINDEX',
       information:
-      """
+      "
 Δικαίωμα για επίδομα τέκνου έχει η οικογένεια της οποίας το ετήσιο ακαθάριστο οικογενειακό εισόδημα για το προηγούμενο έτος δεν υπερβαίνει τις:
 
 - €49.000 για οικογένειες με ένα (1) εξαρτώμενο τέκνο,
@@ -144,15 +136,15 @@ service_data = {
 - €59.000 αυξανόμενες κατά €5.000 για κάθε εξαρτώμενο τέκνο για οικογένειες με περισσότερα από δύο (2) εξαρτώμενα τέκνα.
 
 Δικαίωμα σε επίδομα μονογονεϊκής οικογένειας έχει οικογένεια της οποίας το ετήσιο ακαθάριστο οικογενειακό εισόδημα της για το προηγούμενο έτος δεν υπερβαίνει τις €49.000.
-      """,
-        topics: [ 'help-for-families']
+      ",
+      topics: ['help-for-families']
     },
     en: {
       name: 'Child benefit',
       description: '',
       website: 'https://eforms.eservices.cyprus.gov.cy/mlsi/ApplicationSubmission/SUBMENUINDEX',
       information:
-      """
+      "
 In order to apply for child benefit, the family's gross income for the previous financial year must not exceed:
 
 - €49.000 for one dependant child,
@@ -160,8 +152,8 @@ In order to apply for child benefit, the family's gross income for the previous 
 - €59.000 with a €5.000 for every dependant child for families with more than two children
 
 Single parent families have the right to apply if their gross income for the previous financial year does not exceed €49.000.
-      """,
-        topics: [ 'help-for-families']
+      ",
+      topics: ['help-for-families']
     }
   },
   'childbirth-grant': {
@@ -170,24 +162,24 @@ Single parent families have the right to apply if their gross income for the pre
       description: '',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/B85AA4D88E3630B4C2257B18003B2C9D/$file/%CE%A5%CE%9A%CE%91%203%20-%20006%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%92%CE%9F%CE%97%CE%98%CE%97%CE%9C%CE%91%20%CE%A4%CE%9F%CE%9A%CE%95%CE%A4%CE%9F%CE%A5.pdf',
       information:
-      """
+      "
 Η μητέρα λαμβάνει εφάπαξ ποσό ύψους €573,96 ανά τέκνο (για το έτος 2021), εάν η ίδια ή ο σύζυγός της είναι ασφαλισμένοι, για τη γέννηση ζωντανού παιδιού ή σε περίπτωση θνησιγένειας κατόπιν κύησης διάρκειας τουλάχιστον 28 εβδομάδων.
 
 Για τη χορήγηση του βοηθήματος τοκετού θα πρέπει να υποβληθεί αίτηση, η οποία να συνοδεύεται με τα απαραίτητα πιστοποιητικά εντός ενός (1) έτους από τη γέννηση, ενώ για τη χορήγηση του επιδόματος μητρότητας η αίτηση και τα απαραίτητα πιστοποιητικά θα πρέπει να υποβληθούν μετά το τέλος της 25ης εβδομάδας κύησης και εντός 21 ημερών από την ημερομηνία για την οποία απαιτείται η καταβολή του επιδόματος.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     },
     en: {
       name: 'Childbirth grant',
       description: '',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/B85AA4D88E3630B4C2257B18003B2C9D/$file/%CE%A5%CE%9A%CE%91%203%20-%20006%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%92%CE%9F%CE%97%CE%98%CE%97%CE%9C%CE%91%20%CE%A4%CE%9F%CE%9A%CE%95%CE%A4%CE%9F%CE%A5.pdf',
       information:
-      """
+      "
 A lump sum of €573.96 per child (for the year 2021) is paid to the mother if she or her husband are insured for the birth of a live child or a stillbirth after a pregnancy of at least 28 weeks.
 
 To be awarded the childbirth grant, an application form has to be submitted accompanied by the required certificates within one (1) year of the birth, whereas the maternity allowance to be granted the application form and the required certificates may be submitted after the end of the 25th week of pregnancy and must be submitted within 21 days from the day on which the benefit is required.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     }
   },
   'maternity-pay': {
@@ -196,7 +188,7 @@ To be awarded the childbirth grant, an application form has to be submitted acco
       description: '',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/376ED526792B2965C2257A0900379BF6/$file/%CE%A5%CE%9A%CE%91%203-007%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%95%CE%A0%CE%99%CE%94%CE%9F%CE%9C%CE%91%20%CE%9C%CE%97%CE%A4%CE%A1%CE%9F%CE%A4%CE%97%CE%A4%CE%91%CE%A3.pdf',
       information:
-      """
+      "
 Το επίδομα μητρότητας καταβάλλεται στις βιολογικές μητέρες για χρονικό διάστημα 18 εβδομάδων, αρχής γενομένης μεταξύ της ένατης και της δεύτερης εβδομάδας πριν την εβδομάδα του αναμενόμενου τοκετού.
 
 Εάν το βρέφος χρειαστεί να νοσηλευτεί σε θερμοκοιτίδα λόγω πρόωρου τοκετού ή να λάβει αγωγή εξαιτίας άλλου προβλήματος υγείας, το επίδομα μητρότητας μπορεί να λάβει παράταση μιας εβδομάδας για κάθε 21 ημέρες νοσηλείας που δεν θα υπερβαίνει τη μέγιστη περίοδο των 6 εβδομάδων.
@@ -204,15 +196,15 @@ To be awarded the childbirth grant, an application form has to be submitted acco
 Οι θετές μητέρες λαμβάνουν το επίδομα για 16 εβδομάδες από την εβδομάδα που έγινε η υιοθεσία, εάν το παιδί είναι κάτω των 12 ετών.
 
 Οι παρένθετες μητέρες λαμβάνουν το επίδομα για 14 εβδομάδες, αρχής γενομένης 2 εβδομάδες πριν από την εβδομάδα του αναμενόμενου τοκετού. Σε περίπτωση απόκτησης παιδιού μέσω παρένθετης μητέρας, το επίδομα καταβάλλεται για 18 εβδομάδες, αρχής γενομένης 2 εβδομάδες πριν από την εβδομάδα του αναμενόμενου τοκετού ή από την εβδομάδα του πραγματικού τοκετού, ανάλογα με την επιλογή της αιτήτριας.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     },
     en: {
       name: 'Maternity pay',
       description: '',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/376ED526792B2965C2257A0900379BF6/$file/%CE%A5%CE%9A%CE%91%203-007%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%95%CE%A0%CE%99%CE%94%CE%9F%CE%9C%CE%91%20%CE%9C%CE%97%CE%A4%CE%A1%CE%9F%CE%A4%CE%97%CE%A4%CE%91%CE%A3.pdf',
       information:
-      """
+      "
 Maternity allowance is paid to natural mothers for a period of 18 weeks starting between the ninth and second weeks before the week of the expected birth.
 
 If the infant is treated in an incubator because of premature birth or receives treatment because of another health problem, the maternity benefit may be extended by an additional week for each 21 days of treatment for a maximum of 6 weeks.
@@ -220,8 +212,8 @@ If the infant is treated in an incubator because of premature birth or receives 
 Adoptive mothers receive the allowance for 16 weeks from the week of the adoption if the child is less than 12 years old.
 
 Surrogate mothers receive the allowance for 14 weeks starting 2 weeks before the expected week of confinement. In the case of getting a child through surrogacy, the benefit is paid for a period of 18 weeks starting 2 weeks before the expected week of confinement or from the week of the real confinement, according to the choice of the claimant.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     }
   },
   'paternity-pay': {
@@ -230,24 +222,24 @@ Surrogate mothers receive the allowance for 14 weeks starting 2 weeks before the
       description: 'Επίδομα που παρέχεται σε βιολογικούς και θετούς πατεράδες. ',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/2EEEB0D5888D00F1C225816E00435737/$file/%CE%A5%CE%9A%CE%91%203%20-%20009%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%95%CE%A0%CE%99%CE%94%CE%9F%CE%9C%CE%91%20%CE%A0%CE%91%CE%A4%CE%A1%CE%9F%CE%A4%CE%97%CE%A4%CE%91%CE%A3.pdf',
       information:
-      """
+      "
 Το επίδομα πατρότητας καταβάλλεται στους πατέρες για 2 συνεχείς εβδομάδες, αρχής γενομένης μεταξύ της εβδομάδας του τοκετού και των επόμενων 16 εβδομάδων ή μεταξύ της εβδομάδας του τοκετού και της ολοκλήρωσης της άδειας μητρότητας σε περίπτωση πολλαπλού τοκετού.
 
 Οι θετοί πατέρες λαμβάνουν το επίδομα για περίοδο 2 συνεχών εβδομάδων μεταξύ της εβδομάδας της υιοθεσίας και των επόμενων 16 εβδομάδων.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     },
     en: {
       name: 'Paternity pay',
       description: '',
       website: 'http://www.mlsi.gov.cy/mlsi/sid/sidv2.nsf/All/2EEEB0D5888D00F1C225816E00435737/$file/%CE%A5%CE%9A%CE%91%203%20-%20009%20%CE%91%CE%99%CE%A4%CE%97%CE%A3%CE%97%20%CE%93%CE%99%CE%91%20%CE%95%CE%A0%CE%99%CE%94%CE%9F%CE%9C%CE%91%20%CE%A0%CE%91%CE%A4%CE%A1%CE%9F%CE%A4%CE%97%CE%A4%CE%91%CE%A3.pdf',
       information:
-      """
+      "
 Paternity allowance is paid to fathers for a period of 2 consecutive weeks between the week of confinement and the following 16 weeks or between the week of confinement and the termination of maternity leave for the case of a multiple birth.
 
 Adoptive fathers receive the allowance for a period of 2 consecutive weeks between the week of adoption and the following 16 weeks.
-      """,
-        topics: ['help-for-families']
+      ",
+      topics: ['help-for-families']
     }
   }
 }
